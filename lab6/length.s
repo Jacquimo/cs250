9d907654fc2fbf0b7d688bf4b559c4a3b36dc182
Gray Houston <root@raspberrypi.(none)>
2014-10-21 18:07:10 -0400

    Got length to work using he stack. Updated everything to account for this

--- lab6/length.s
+++ lab6/length.s
@@ -1,67 +1,58 @@
 	.text
 	.global main
 main:
-	stmed sp!, {fp, lr} // push return info onto stack
-	add fp, sp, #4 // set fp to be 1 byte above sp on stack
-
-	sub r1, fp, #4 // set r1 to point to first local variable
-	ldr r0, =readFormat // set r0 to format string
-	bl scanf // scan in the value
-	sub r1, fp, #4 // set r1 to point to the newly read string
-
-	/* ldr r0, =printFormat
-	sub r1, fp, #4
-	bl printf */
-
-	//ldr r0, =testString
-	//bl printf
-
-
-	mov r2, #0 // initialize a counter variable in r2
-loop:	
-	//add r1, r1, #1 // increment r1 to the next character
-	//ldr r1, [[r1]] // get the character value at the start of the string
-	//ldr r0, =testFormat
-	//bl printf
+	// Initialize function call
+	push {fp, lr} // push return variables onto the stack
+	mov fp, sp // set the frame pointer to the stack pointer
 	
-	ldr r3, [r1] // assign character value in string to r3
-	//ldr r3, [r3]
-	cmp r3, #0 // check to see if it's the null character
-break1:	
-	beq endloop // if the condition is met, end the loop
-
-	// test the values
-	//mov r4, r1
-	//mov r1, r3
-	//ldr r0, =testFormat
-	//bl printf
-	//mov r1, r4
+	// Scan in the input
+	sub sp, sp, #400 // allocate memory for a local variable
+	mov r1, sp // set r1 to the location of sp
+	ldr r0, =readfmt // load format string into r0
+	bl scanf // call scanf and scan in the string from stdin
 	
-	add r2, r2, #1 // increment the counter by 1
-	add r1, r1, #1 // increment pointer to next character
-	b loop // branch back to the start of the loop
+	// Initialize variables for loop
+	mov r6, #0 // initialize counter variable "int i = 0"
+break2:	mov r4, sp // set r4 to point to first character in string
 	
-endloop: 
+	/* r3 = counter, r4 = location on stack, r5 = char*, r6 = ascii value
+	Start the loop */
+loop:
+	// Get the next ascii value
+	ldrb r5, [r4] // get the actual string pointer
 
-	// At this point, we have looped over the entire string
-	mov r1, r2 // move the value in the counter to r1
-	ldr r0, =printFormat // set r0 to the format string
-	bl printf
+	// Test ascii value by printing it out
+	//ldr r0, =testing
+	//mov r1, r5
+	//bl printf	
 
-	// Call memdump to find my errors
-	//mov r0, sp // set r0 to point to the stack pointer
-	//mov r1, #64 // set length for memdump
-	//bl memdump
+	// If the ascii value is null, end the loop
+	cmp r5, #0
+	beq endloop
 	
-	// prepare for returning to caller function
+	// Update vars
+	add r6, r6, #1 // update the counter variable
+	add r4, r4, #1 // move pointer to next character
+
+	// Return to the start of the loop
+break1:	b loop
+	
+endloop:
+
+	// Print output
+	ldr r0, =printfmt // load format string into r0
+	mov r1, r6 // load counter variable into r1
+break3:	bl printf // print the output
+	
+	// Return to caller function
 	mov sp, fp
-	ldmfd sp!, {fp, pc}
+	pop {fp, lr}
 	bx lr
 
 
 	.section	.rodata
-readFormat:	.asciz "%s"
-printFormat:	.asciz "The length of the string is %d\n"
-testFormat:	.asciz "%c\n"
-testString:	.asciz "%s\n"
+readfmt:	.asciz "%99[^\n]"
+printfmt:	.asciz "The length of the string is: %d\n"
+testing:	.asciz "%c\n"
 
+	
