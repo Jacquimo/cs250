5729a8a71529972d1c324e10b7a39c8825ded438
Gray Houston <ghousto@purdue.edu>
2014-12-04 11:21:14 -0500

    Working on bubble sort loop

--- lab11/bubble.s
+++ lab11/bubble.s
@@ -3,5 +3,21 @@
 bubble:
 	enter $(8 * 0), $0
 
+	mov $0, %r12 /* initialize counter variable */
+
+outerLoop:
+	cmp $1, (%rdi) /* see if the the number of elements to iterate over is <= 1 */
+	jle endOuter
+
+	mov (%rdi), %r8
+	add $-1, %r8
+	mov %r8, (%rdi)
+	jmp outerLoop
+endOuter:
+
 	leave
 	ret
+
+
+printFmt:
+	.string "%d\n"
